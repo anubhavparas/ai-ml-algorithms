1dfb9a03bff659258de36b609f70e296fc6f5efb
akhopkar01 <aadi0110@gmail.com>
2020-03-02 09:46:19 -0800

    Made required changes

--- dijkstra/Workspace_Map.py
+++ dijkstra/Workspace_Map.py
@@ -10,40 +10,40 @@ import pygame as pg
 import cv2
 
 #pg.init()
-class drawMap:
+class WorkspaceMap:
     def __init__(self,radius,clearance):
         
-        self.R = radius
-        self.C = clearance
+        self.radius = radius
+        self.clearance = clearance
         self.height = 200
         self.width = 300
         #self.Surface = pg.display.set_mode((self.width,self.height))
-        self.ax = self.R+self.C
+        self.shift = self.radius+self.clearance
     
     def getMap(self,window):
-        blue = (0,0,255)
-        white = (255,255,255)
-        window.fill(white)
-        coord_Poly = [(20-self.ax,self.height-(120-self.ax)),
-                      (25-self.ax,self.height-(185+self.ax)),
-                      (75+self.ax,self.height-(185+self.ax)),
-                      (100+self.ax,self.height-(150+self.ax)),
-                      (75+self.ax,self.height-(120-self.ax)),
-                      (50,self.height-(150-self.ax))]
-        coord_rect = [(30-self.ax,self.height-(67.5+self.ax)),
-                      (35+self.ax,self.height-(76+self.ax)),
-                      (100+self.ax,self.height-(38.6-self.ax)),
-                      (95-self.ax,self.height-(30-self.ax))]
+        BLUE_COLOR = (0,0,255)
+        BLACK_COLOR = (0,0,0)
+        window.fill(BLACK_COLOR)
+        coord_Poly = [(20-self.shift,self.height-(120-self.shift)),
+                      (25-self.shift,self.height-(185+self.shift)),
+                      (75+self.shift,self.height-(185+self.shift)),
+                      (100+self.shift,self.height-(150+self.shift)),
+                      (75+self.shift,self.height-(120-self.shift)),
+                      (50+self.shift,self.height-(150-self.shift))]
+        coord_rect = [(30-self.shift,self.height-(67.5+self.shift)),
+                      (35+self.shift,self.height-(76+self.shift)),
+                      (100+self.shift,self.height-(38.6-self.shift)),
+                      (95-self.shift,self.height-(30-self.shift))]
         #circle = [(25+self.ax,225)]
-        coord_rhom = [(225,self.height-(40+self.ax)),
-                      (250+self.ax,self.height-25),
-                      (225,self.height-(10-self.ax)),
-                      (200-self.ax,self.height-25)]
-        poly1 = pg.draw.polygon(window,blue,coord_Poly,0)
-        poly2 = pg.draw.polygon(window,blue,coord_rect,0)
-        poly3 = pg.draw.polygon(window,blue,coord_rhom,0)
-        circle = pg.draw.circle(window,blue,(225,self.height-125),25+self.ax,0)
-        ellipse = pg.draw.ellipse(window,blue,(150,self.height-100,80+self.ax,40+self.ax),0)
+        coord_rhom = [(225,self.height-(40+self.shift)),
+                      (250+self.shift,self.height-25),
+                      (225,self.height-(10-self.shift)),
+                      (200-self.shift,self.height-25)]
+        poly1 = pg.draw.polygon(window,BLUE_COLOR,coord_Poly,0)
+        poly2 = pg.draw.polygon(window,BLUE_COLOR,coord_rect,0)
+        poly3 = pg.draw.polygon(window,BLUE_COLOR,coord_rhom,0)
+        circle = pg.draw.circle(window,BLUE_COLOR,(225,self.height-125),25+self.shift,0)
+        ellipse = pg.draw.ellipse(window,BLUE_COLOR,(150,self.height-100,80+self.shift,40+self.shift),0)
         Map_ = [poly1,poly2,poly3,circle,ellipse]
         return Map_
         
@@ -52,7 +52,7 @@ class drawMap:
 # In[ ]:
 
 
-m = drawMap(2,1)
+m = WorkspaceMap(2,1)
 pg.init()
 window = pg.display.set_mode((300,200))
 MAP = m.getMap(window)
