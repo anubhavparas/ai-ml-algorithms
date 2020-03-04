45f988851095961d194645ec4e8bad00f21bf1fa
Anubhav Paras <anubhp@users.noreply.github.com>
2020-03-04 17:21:08 -0500

    made a local or operation predicate

--- dijkstras_path_finder/configurationspace.py
+++ dijkstras_path_finder/configurationspace.py
@@ -19,18 +19,17 @@ class ConfigurationSpace:
         img = np.zeros((height, width, 3), dtype=np.uint8)
 
         c_space_predicates = CSpacePredicateSupplier().get_cspace_predicates(height, width, padding)
+        predicate_or_op = lambda predicate1, predicate2: predicate1 or predicate2 
+
         for y in range(0, height):
             for x in range(0, width):
-                is_point_in_obstacle = reduce(self.predicate_or_op, [c_space_predicate(x,y) for c_space_predicate in c_space_predicates])
+                is_point_in_obstacle = reduce(predicate_or_op, [c_space_predicate(x,y) for c_space_predicate in c_space_predicates])
                 if is_point_in_obstacle:
                     img[y, x] = BLUE_COLOR
 
         print('Configuration space initialized.\n')
         return img
 
-
-    def predicate_or_op(self, predicate1, predicate2):
-        return predicate1 or predicate2
 
 
     
